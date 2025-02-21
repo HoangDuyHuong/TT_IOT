cf3b521d7cda99b261407ec46cbd58a0c407c1be
HoangDuyHuong <dellcook69@gmail.com>
2025-02-21 07:51:29 +0700

    update

---
+++ Giao_Dien_lamp/control.js
@@ -0,0 +1,30 @@
+
+
+switch_01.addEventListener('change', () => {
+    if (switch_01.checked) {
+        img_01.src = 'img/light_on.png';
+    } else {
+        img_01.src = 'img/light_off.png';
+    }
+});
+switch_02.addEventListener('change', () => {
+    if (switch_02.checked) {
+        img_02.src = 'img/light_on.png';
+    } else {
+        img_02.src = 'img/light_off.png';
+    }
+});
+switch_03.addEventListener('change', () => {
+    if (switch_03.checked) {
+        img_03.src = 'img/light_on.png';
+    } else {
+        img_03.src = 'img/light_off.png';
+    }
+});
+switch_04.addEventListener('change', () => {
+    if (switch_04.checked) {
+        img_04.src = 'img/light_on.png';
+    } else {
+        img_04.src = 'img/light_off.png';
+    }
+});
